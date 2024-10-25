d2eb5523f4584475e7d9f0981ee385507d9e7b17
Satyam Shorrf <160311244+satyamshorrf@users.noreply.github.com>
2024-10-25 06:15:26 +0530

    initial commit

---
+++ nav.js
@@ -0,0 +1,16 @@
+// disable right click
+// document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility == "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility == "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
